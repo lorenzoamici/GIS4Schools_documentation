97b0d1a105b91fc340c8d47265a922f5774254fa
lawfriends <lawfriends12@gmail.com>
2021-02-25 12:01:01 +0100

    added logo, license and modified background color

--- conf.py
+++ conf.py
@@ -66,15 +66,18 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'template*', 'old']
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_logo = './img/G4S-p1-hor.svg'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-    'logo_only': False,
+    'logo_only': True,
     'style_external_links': True,
     'collapse_navigation': False,
     'includehidden': False,
+    'style_nav_header_background': '#55a1c9',
 }
 
 
@@ -82,6 +85,8 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = ['css/my_theme.css']
 
 # code for making links open in a new tab
 
