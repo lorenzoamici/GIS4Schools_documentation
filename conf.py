741f9e3d957b521607239594214c5a92385aabf4
lawfriends <lawfriends12@gmail.com>
2021-02-25 12:46:19 +0100

    fix

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ from docutils.nodes import Element
 
 
 def setup(app):
-    app.add_css_file('/my_theme.css')
+    app.add_css_file('my_theme.css')
 
 
 # -- Project information -----------------------------------------------------
